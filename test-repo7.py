015779284a1fb42e0cf50ef0bda3a1e319c84a06
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-06-26 23:44:08 -0800

    Create test-repo7.py

---
+++ test-repo7.py
@@ -0,0 +1,7 @@
+# Comparison operator to compare value
+price = 25
+print(price > 10 and price < 30)    # boolean expression is true
+price = 5
+print(price > 10 or price < 30)     # boolean expression is true
+price = 5
+print(not price > 10)               # boolean expression is true
